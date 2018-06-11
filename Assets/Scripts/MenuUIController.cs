2b24d91a304e9027d7568b111e9ab00a8fc6d36f
Rich Hylands <richardhy@unity3d.com>
2018-06-11 12:46:23 +0100

    Initial commit

---
+++ Assets/Scripts/MenuUIController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIController : MonoBehaviour {
+    public bool OnStartSwitchToMain = true;
+	public GameObject MainMenu;
+	public GameObject SettingsMenu;
+
+	public void Awake()
+	{
+        if (OnStartSwitchToMain)
+        {
+            EnableMainMenu();
+            DisableSettingsMenu();
+        }
+	}
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene("Main", LoadSceneMode.Single);
+		// SceneManager.UnloadSceneAsync("UI");
+		// MainMenu.SetActive(false);
+		Debug.Log(MainMenu.activeInHierarchy);
+	}
+	public void EnableMainMenu()
+	{
+		MainMenu.SetActive(true);
+	}
+	public void DisableSettingsMenu()
+	{
+		SettingsMenu.SetActive(false);
+	}
+	public void EnableSettingsMenu()
+	{
+		SettingsMenu.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoad
+{
+    public static void LoadScene()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+    }
+}
